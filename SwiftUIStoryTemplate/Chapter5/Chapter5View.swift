3124b71c8fe255e5c130e0036e09e42fa8e3e41b
Cards <dariusjackson50@gmail.com>
2023-11-16 16:28:05 -0500

    final product chpt2

--- SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
+++ SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
@@ -1,11 +1,27 @@
 import SwiftUI
 
 struct Chapter5View: View {
+    private let colors: [Color] = [.red, .blue, .green, .orange, .black]
+    
     var body: some View {
-        Text("Chapter 5")
+        VStack{
+            TabView{
+                ForEach(colors, id: \.self) {
+                    color in
+                    ZStack {
+                        color
+                        Text("\(color.description)")
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+                }
+                
+    }
+            .tabViewStyle(.page)
+        }
     }
 }
-
-#Preview {
-    Chapter5View()
-}
+        #Preview {
+            Chapter5View()
+        }
+    
